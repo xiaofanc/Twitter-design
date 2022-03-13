615a83922234f4cfb1cdcae122316229b39648cb
xiaofanc <xfang.chu@gmail.com>
2022-03-12 23:28:15 -0800

    remove unused lib

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -1,7 +1,5 @@
-from crypt import methods
-from pickle import TRUE
 from rest_framework.response import Response
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from django.contrib.auth.models import User
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
