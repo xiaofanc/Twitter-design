caa6bb662d80cc9b449131af0cc9b709a43f9ec6
xiaofanc <xfang.chu@gmail.com>
2022-03-28 16:08:35 -0700

    delete model teacher - test

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from accounts.models import Teacher
-
-class TeacherAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name', 'date_of_birth')
-
-admin.site.register(Teacher, TeacherAdmin)
